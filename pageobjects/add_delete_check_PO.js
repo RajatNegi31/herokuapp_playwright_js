f5af4fa263a3927a49288619984b5a07175838dc
RajatNegi31 <rajat.negi67@gmail.com>
2024-03-07 09:22:37 +0530

    added

--- pageobjects/add_delete_check_PO.js
+++ pageobjects/add_delete_check_PO.js
@@ -5,19 +5,19 @@ constructor(page)
     this.page= page;
     this.add_btn= page.locator("button[onclick='addElement()']")
     this.delete_btn= page.locator("button[class='added-manually']")
-
-
 }
 
-async add_btn(add_btn,delete_btn){
+async LandOnPage(){
+    await this.page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+}
+
+async add_btn(){
     await this.add_btn.click();
 }
 
-async delete_btn(dd_btn,delete_btn){
-    await delete_btn.click()
+async delete_btn(){
+    await this.delete_btn.click()
 }
-
-
 }
 
 module.exports={add_delete_check_PO};
--- tests/add_delete_check.spec.js
+++ tests/add_delete_check.spec.js
@@ -1,16 +1,20 @@
 const {test, expect}=require('@playwright/test')
+const {add_delete_check_PO}=require('../pageobjects/add_delete_check_PO');
 
 
 const url= "https://the-internet.herokuapp.com/add_remove_elements/";
 test('Test_1', async({browser})=>{
 
     const context=await browser.newContext();
-    const page=context.newPage();
+    const page=await context.newPage();
 
-    const add_btn=await page.locator("button[onclick='addElement()']")
-    const delete_btn=await page.locator("button[class='added-manually']")
+    //const add_btn=await page.locator("button[onclick='addElement()']")
+    //const delete_btn=await page.locator("button[class='added-manually']")
 
-    const add_delete_check_PO=new add_delete_check_PO();
+    const add_delete_check_PO=new add_delete_check_PO(page);
 
-
-})
+    await add_delete_check_PO.LandOnPage();
+    await add_delete_check_PO.add_btn();
+    await add_delete_check_PO.delete_btn();
+}
+)
